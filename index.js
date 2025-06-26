9a113a869002b7a9c751c27e948bdd433aae6496
Shubham <realitymaniac7@gmail.com>
2025-06-27 03:20:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config()
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("welcome to the server")
+})
+
+app.get('/shubham', (req, res) => {
+    res.send("<h1> My name is shubham </h1>")
+})
+
+app.get('/github', (req, res) => {
+    res.send("<h1><a href=\"https://github.com/shubhamxpawar\"> Github </a> </h1>")
+})
+
+app.listen(process.env.PORT, () => {console.log(`server started on port ${process.env.PORT}`)})
