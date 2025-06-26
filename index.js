48a24651d522061785702eb4cdbae71b7e5f2dc3
Shubham <realitymaniac7@gmail.com>
2025-06-27 03:26:58 +0530

    port

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config()
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.get('/', (req, res) => {
     res.send("welcome to the server")
 })
